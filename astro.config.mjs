19754bcfe09992501b482ced66153104082c00d0
Juan Pablo <juanpa.guayara@outlook.com>
2024-11-02 21:49:48 -0500

    Implementacion del despliegue continuo con github actions, que implico cambios en las rutas, ademas crear el yml para hacer el despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://Guayixxx.github.io',
+  base: 'Porfolio',
   integrations: [mdx(), sitemap(), tailwind()]
 });
